8e352f296b01484573fb0eeb1e8c79a83fec62fd
Ezz Khayyat <ekhayyat@gmail.com>
2017-11-05 18:59:10 +0200

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+$(document).foundation();
+
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>Boiletplate 3 Project</p>,
+  document.getElementById('app')
+);
