394c2c22fcfe61ddc6b56d17048f74fa365ebaf5
Ezz Khayyat <ekhayyat@gmail.com>
2017-11-08 20:56:33 +0200

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -16,5 +16,30 @@ module.exports = {
     } catch(e) {  }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      return todo.text.toLowerCase().includes(searchText.toLowerCase());
+    });
+
+    // sort todos with non-completed fisrt
+    filteredTodos = filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed)
+        return -1;
+      else if (a.completed && !b.completed)
+          return 1;
+      return 0;
+
+    });
+
+    return filteredTodos;
   }
 };
