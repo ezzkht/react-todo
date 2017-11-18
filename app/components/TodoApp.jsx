6fd052e1aaea8184da0bd459d57b217afccda472
Ezz Khayyat <ekhayyat@gmail.com>
2017-11-18 11:02:59 +0200

    Refactor app to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,47 +2,12 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-//var TodoList = require('TodoList');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function() {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-    };
-  },
-  componentDidUpdate: function() {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleAddTodo: function(text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-  });
-  },
-  handleSearch: function(showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
   render: function() {
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-
     return (
       <div style={{width:40 + '%'}}>
         <h1 className="page-title">Todo App</h1>
@@ -50,9 +15,9 @@ var TodoApp = React.createClass({
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <TodoSearch onSearch={this.handleSearch}></TodoSearch>
+              <TodoSearch></TodoSearch>
               <TodoList/>
-              <AddTodo onAddTodo={this.handleAddTodo}/>
+              <AddTodo/>
             </div>
           </div>
         </div>
--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -22,6 +22,26 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate add todos action', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }
+    ];
+
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should generate toggle show completed action', () => {
     var action = {
       type: 'TOGGLE_SHOW_COMPLETED'
